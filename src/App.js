bc20fef1bcf97f9016ec5f1f1ff32cfb9f1ae97c
Adam Bedient <adambedient@gmail.com>
2024-04-13 19:57:04 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [win, setWin] = useState(false)
+
+
+  function handleKeyPress(event) {
+    if (event.key === 'a') {
+      setWin(true)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keypress', handleKeyPress)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {win ? <p>you win</p> : <p>type A</p>}
+    </main>
   );
 }
 
